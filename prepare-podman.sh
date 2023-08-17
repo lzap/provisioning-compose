020cdd337d22e6db04475b42c8525497b9aa5f12
Lukas Zapletal <lukas-x@zapletalovi.com>
2023-08-17 15:39:20 +0200

    Update prepare podman script

--- prepare-podman.sh
+++ prepare-podman.sh
@@ -2,4 +2,4 @@
 
 podman unshare rm -rf ./data/*
 mkdir -p ./data/{redis,pg-backend,pg-sources,pg-notifications,zookeeper,kafka}
-podman unshare chown -R 1001:1001 ./data/kafka ./data/zookeeper
+podman unshare chown -R $(id -u):$(id -g) ./data/kafka ./data/zookeeper
